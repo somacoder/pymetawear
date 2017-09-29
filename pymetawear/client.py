9e5e09a724ca8faaedaa2e2029966d0c5a5e9e83
somacoder <gkbartleyjr@gmail.com>
2017-09-29 16:22:24 -0400

    Update client.py

--- pymetawear/client.py
+++ pymetawear/client.py
@@ -12,6 +12,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from ctypes import byref, cast, c_ubyte, c_uint, POINTER
+import errno
 import logging
 import os
 
@@ -24,7 +26,20 @@ from pymetawear.mbientlab.metawear.cbindings import Const
 
 log = logging.getLogger(__name__)
 
-
+_model_names = [
+    "Unknown",
+    "MetaWear R",
+    "MetaWear RG",
+    "MetaWear RPro",
+    "MetaWear C",
+    "MetaWear CPro",
+    "MetaEnvironment",
+    "MetaDetector",
+    "MetaHealth",
+    "MetaTracker",
+    "MetaMotion R",
+    "MetaMotion C"
+]
 class MetaWearClient(object):
     """A MetaWear communication client.
 
@@ -118,6 +133,41 @@ class MetaWearClient(object):
     def board(self):
         return self.backend.board
 
+    @staticmethod
+    def _lookup_path(path):
+        return path if path is not None else ".metawear"
+
+    @staticmethod
+    def _lookup_name(name, address):
+        return name if name is not None else address.replace(':','')
+
+    def serialize(self, path=None, name=None):
+        realpath = MetaWearClient._lookup_path(path)
+        try:
+            os.makedirs(realpath)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
+        finally:
+            size = c_uint(0)
+            state = cast(libmetawear.mbl_mw_metawearboard_serialize(self.board, byref(size)), POINTER(c_ubyte * size.value))
+            python_array= []
+            for i in range(0, size.value):
+                python_array.append(state.contents[i])
+
+            realfile = MetaWearClient._lookup_name(name, self._address)
+            with open(os.path.join(os.getcwd(), realpath, realfile), "wb") as file:
+                file.write(state.contents)
+            libmetawear.mbl_mw_memory_free(state)
+
+    def deserialize(self, path=None, name=None):
+        state_path = os.path.join(os.getcwd(), MetaWearClient._lookup_path(path), MetaWearClient._lookup_name(name, self._address)) 
+        if os.path.isfile(state_path):
+            with(open(state_path, "rb")) as f:
+                content = f.read()
+                raw = (c_ubyte * len(content)).from_buffer_copy(content)
+                libmetawear.mbl_mw_metawearboard_deserialize(self.board, raw, len(content))
+
     def connect(self, clean_connect=False):
         """Connect this client to the MetaWear device.
 
@@ -144,18 +194,22 @@ class MetaWearClient(object):
                     self.backend.initialization_status))
 
         # Read out firmware and model version.
-        self.firmware_version = tuple(
-            [int(x) for x in self.backend.read_gatt_char_by_uuid(
-                specs.DEV_INFO_FIRMWARE_CHAR[1]).decode().split('.')])
+        self.firmware_version_str = self.backend.read_gatt_char_by_uuid(specs.DEV_INFO_FIRMWARE_CHAR[1]).decode()
+        self.firmware_version = tuple([int(x) for x in self.firmware_version_str.split('.')])
         self.model_version = int(self.backend.read_gatt_char_by_uuid(
             specs.DEV_INFO_MODEL_CHAR[1]).decode())
+        self.model_name = _model_names[libmetawear.mbl_mw_metawearboard_get_model(self.board) + 1]
 
         self._initialize_modules()
 
-    def disconnect(self):
-        """Disconnects this client from the MetaWear device."""
-        libmetawear.mbl_mw_metawearboard_tear_down(self.board)
+    def free(self):
+        """Frees unmanaged memory used by thie object"""
         libmetawear.mbl_mw_metawearboard_free(self.board)
+
+    def disconnect(self, tear_down=True):
+        """Disconnects this client from the MetaWear device."""
+        if tear_down:
+            libmetawear.mbl_mw_metawearboard_tear_down(self.board)
         self.backend.disconnect()
 
     def get_handle(self, uuid, notify_handle=False):
